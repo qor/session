310f27d9b4958d99e9ea20e329b5541543981f10
Jinzhu <wosmvp@gmail.com>
2017-09-06 11:25:00 +0800

    Update SCS with latest v1.0 release

--- scs/scs_test.go
+++ scs/scs_test.go
@@ -3,13 +3,14 @@ package scs_test
 import (
 	"testing"
 
-	"github.com/alexedwards/scs/engine/memstore"
+	scssession "github.com/alexedwards/scs"
+	"github.com/alexedwards/scs/stores/memstore"
 	"github.com/qor/session/scs"
 	"github.com/qor/session/test"
 )
 
 func TestAll(t *testing.T) {
-	engine := memstore.New(0)
+	engine := scssession.NewManager(memstore.New(0))
 	manager := scs.New(engine)
 	test.TestAll(manager, t)
 }
